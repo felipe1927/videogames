768a04455a6ec10dca6760defa10d3753e05c3f6
sebastian cordoba <sebastian.cordoba@phidias.com.co>
2016-12-28 14:26:12 -0500

    menu

--- src/states/Index.vue
+++ src/states/Index.vue
@@ -1,16 +1,27 @@
 <template>
 	<div id="state-index">
+		<!-- cabecera -->
 		<header>
 			<div class="container">
-				<h1>Titulo</h1>
-				<router-link to="/">Home</router-link>
+				<p><span class="glyphicon glyphicon-menu-hamburger"></span></p>
+				<p>Logo</p>
+
+				<ul>
+					<li><a href="">Xbox</a></li>
+					<li><a href="">Play</a></li>
+					<li><a href="">Pc</a></li>
+				</ul>
 			</div>
 		</header>
+
+		<!-- cuerpo -->
 		<div class="body">
-			<div class="container">
+			<div class="container cont-body">
 				<router-view></router-view>				
 			</div>
 		</div>
+
+		<!-- pie de pagina -->
 		<footer>
 			<div class="container">
 				<h2>footer</h2>
@@ -28,14 +39,72 @@
 <style lang="scss">
 	#state-index{
 		header{
-			background: #2c3e50;
-			color: #fff;
+			p:nth-child(1){
+				display: none;
+			}
+			p{
+				float: left;
+				font-weight: bold;
+				margin: 0;
+				padding: 15px;
+				-webkit-box-sizing: border-box;
+				-moz-box-sizing: border-box;
+				box-sizing: border-box;
+			}
+			ul{
+				margin: 0;
+				padding: 0;
+				width: 50%;
+				float: right;
+				list-style: none;
+				text-align: right;
+				li{
+					display: inline-block;
+					a{
+						color: #fff;
+						text-decoration: none;
+						display: inline-block;
+						padding: 15px;
+						-webkit-box-sizing: border-box;
+						-moz-box-sizing: border-box;
+						box-sizing: border-box;
+
+					}
+				}
+			}
 		}
 
-		footer {
-			background: #2c3e50;
-			color: #fff;
+		.cont-body{
+			background: #fff;
 		}
+
+
+
+		@media only screen and (max-width : 600px) {
+			header{
+				p:nth-child(1){
+					display: block;
+					cursor: pointer;
+				}
+				ul{
+					top: 0;
+					left: -230px;
+					width: 230px;
+					padding: 0;
+					height: 100%;
+					position: fixed;
+					text-align: left;
+					background: #2C3E50;
+					li{
+						display: block;
+						a{
+							width: 100%;
+						}
+					}
+				}
+			}
+		}
+		
 
 	}
 </style>
