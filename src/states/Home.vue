497b1b8d146d53cf9620028c0e2ea9c9cef8413e
sebastian cordoba <sebastian.cordoba@phidias.com.co>
2016-12-29 14:38:00 -0500

    home

--- src/states/Home.vue
+++ src/states/Home.vue
@@ -19,8 +19,19 @@
 
 			<div class="col-md-4">
 				<titulos name="Proximos juegos" icon="glyphicon-fire"></titulos>
-				<div class="panels">
-					arreglo
+				<div class="panels proximos_juegos">
+					<div v-for="item in proximosJuegos">
+						<div class="row">
+							<div class="col-md-4">
+								<div class="imagen"></div>
+							</div>
+							<div class="col-md-8">
+								<div class="titulo">{{ item.titulo }}</div>
+								{{ item.resumen }}
+							</div>
+						</div>
+						<hr>
+					</div>
 				</div>
 
 				<br>
@@ -45,7 +56,14 @@
 
 <script>
 	export default{
-
+		data () {
+			return {
+				proximosJuegos:[
+					{titulo: 'Titulo', resumen: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta dolore, similique velit. Modi explicabo harum inventore quibusdam error iusto atque quo debitis, in labore...'},
+					{titulo: 'Titulo', resumen: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta dolore, similique velit. Modi explicabo harum inventore quibusdam error iusto atque quo debitis, in labore...'},
+				]
+			}
+		}
 	}
 </script>
 
@@ -57,6 +75,21 @@
 			background: url(../assets/wall2.png)center center;
 			-webkit-background-size: cover;
 			background-size: cover;
+		}
+
+		.proximos_juegos{
+			font-size: 11px;
+			.imagen{
+				width: 100%;
+				height: 80px;
+				background: #414141;
+			}
+			.titulo{
+				width: 100%;
+				border-bottom: 1px solid #414141;
+				margin-bottom: 5px;
+				font-weight: bold;
+			}
 		}
 	}	
 </style>
