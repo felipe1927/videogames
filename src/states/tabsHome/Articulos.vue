c1b75422145bcd9e5dd70cbc9d6022ee24df2e05
sebastian cordoba <sebastian.cordoba@phidias.com.co>
2017-01-05 11:53:18 -0500

    articulos

--- src/states/tabsHome/Articulos.vue
+++ src/states/tabsHome/Articulos.vue
@@ -4,7 +4,7 @@
 		<div class="row">
 			<div class="col-md-4 col-xs-12 col-sm-4" v-for="item in articulos">
 				<images :url="item.img" x="100%" y="150px"></images>
-				<div class="bold"><a href="">{{ item.titulo }}</a></div>
+				<div class="bold"><a href="">{{ item.titulo }}</a> <br> <ticket name="Articulos" id="articulo"></ticket></div>
 				<br>
 			</div>
 		</div>
